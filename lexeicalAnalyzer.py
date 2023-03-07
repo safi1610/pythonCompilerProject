9e78c4d2689dfaea2725480a6b0d3702847c6175
safi1610 <safi1610@mylaurier.ca>
2023-03-07 10:05:31 -0500

    Added cases for identifier search

--- lexeicalAnalyzer.py
+++ lexeicalAnalyzer.py
@@ -5,12 +5,24 @@ types = ["int", "double"]
 
 alphabet = string.ascii_lowercase + string.ascii_uppercase
 
+symbol = ""
+
 state = 0
 def getNextToken(token):
     for element in token:
         match state:
             case 0:
                 if element in alphabet and element.islower():
+                    symbol += element
                     state = 1
                 break
+            case 1:
+                if element == "\s" or element == "\t" or element == "\n":
+                    state = 0
+
+                if element in alphabet or element.isDigit():
+                    symbol += element
+
+                break
+
             
