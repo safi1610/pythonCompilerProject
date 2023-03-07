13ccbf5f80b273386d8ba48083120f73f6abfd1f
safi1610 <safi1610@mylaurier.ca>
2023-03-07 01:29:29 -0500

    Made changes to lexer

--- lexeicalAnalyzer.py
+++ lexeicalAnalyzer.py
@@ -1,3 +1,16 @@
+import string
+
+keyWords = ["if", "fi", "while", "do", "od", "def", "fed", "print"]
+types = ["int", "double"]
+
+alphabet = string.ascii_lowercase + string.ascii_uppercase
+
+state = 0
 def getNextToken(token):
-    keyWords = ["if", "fi", "while", "do", "od", "def", "fed", "print"]
-    types = ["int", "double"]
+    for element in token:
+        match state:
+            case 0:
+                if element in alphabet and element.islower():
+                    state = 1
+                break
+            
