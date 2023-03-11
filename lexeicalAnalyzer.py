7c48332b3383fbea31d81a083450f70c35d61d4a
safi1610 <safi1610@mylaurier.ca>
2023-03-10 23:46:53 -0500

    Added checks for delimeters in order to make toks

--- lexeicalAnalyzer.py
+++ lexeicalAnalyzer.py
@@ -1,36 +1,81 @@
 import string
 import token_1 as t
 
-keyWords = ["if", "fi", "while", "do", "od", "def", "fed", "print"]
+keyWords = ["if", "fi", "while", "do", "od", "def", "fed", "print", "then"]
 types = ["int", "double"]
+operators = ["+", "-", "*", "/", "%", "="]
+
 
 alphabet = string.ascii_lowercase + string.ascii_uppercase
 
 # state = 0
 def getNextToken(token):
+    classification = ""
     symbol = ""
     state = 0
+    delTok = ""
     for element in token:
-        if element == " " or element == r"\t" or element == r"\n":
-            tk = t.Token(symbol, "token")
-            state = 0
-            symbol = ""
-
-            print(tk.element, tk.classification)
-
-            continue
-
 
         match state:
+            
             case 0:
+                if delTok == " ":
+                    delTok = ""
+
+                if delTok == ",":
+                    symbol += delTok
+                    classification = "comma"
+                    tk = t.Token(symbol, classification)
+                    print(tk.element, tk.classification)
+
+                if delTok == ";":
+                    symbol += delTok
+                    classification = "semi-colon"
+                    tk = t.Token(symbol, classification)
+                    print(tk.element, tk.classification)
+
+                if delTok == "(":
+                    symbol += delTok
+                    classification = "opening parentheses"
+                    tk = t.Token(symbol, classification)
+                    print(tk.element, tk.classification)
+
+                if delTok == ")":
+                    symbol += delTok
+                    classification = "closing parentheses"
+                    tk = t.Token(symbol, classification)
+                    print(tk.element, tk.classification)
+
+                if delTok in operators:
+                    symbol += delTok
+                    classification = "operator"
+                    tk = t.Token(symbol, classification)
+                    print(tk.element, tk.classification)
+                
+                symbol = ""
+                delTok = ""
+
                 if element in alphabet and element.islower():
                     symbol += element
                     state = 1
+
                 # break
             case 1:
 
-                if element in alphabet or element.isDigit():
+                if element in alphabet or element.isdigit():
                     symbol += element
+                else:
+                    if symbol in keyWords:
+                        classification = "keyword"
+                    elif symbol in types:
+                        classification = "type"
+                    else:
+                        classification = "identifier"
+                    tk = t.Token(symbol, classification)
+                    symbol = ""
+                    delTok += element
+                    print(tk.element, tk.classification)
+                    state = 0
 
                 # break
 
