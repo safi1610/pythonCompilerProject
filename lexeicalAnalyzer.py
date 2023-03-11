f762dd3a89328042897a49fbcc0b5cc8d4fc8e92
safi1610 <safi1610@mylaurier.ca>
2023-03-11 14:46:38 -0500

    changed from using for to while loop for iterate

--- lexeicalAnalyzer.py
+++ lexeicalAnalyzer.py
@@ -14,69 +14,20 @@ def getNextToken(token):
     symbol = ""
     state = 0
     delTok = ""
-    for element in token:
+    curr = 0
+    buffLen = token.len()
 
+    while curr != buffLen:
         match state:
-            
             case 0:
-                if delTok == " ":
-                    delTok = ""
-
-                if delTok == ",":
-                    symbol += delTok
-                    classification = "comma"
-                    tk = t.Token(symbol, classification)
-                    print(tk.element, tk.classification)
-
-                if delTok == ";":
-                    symbol += delTok
-                    classification = "semi-colon"
-                    tk = t.Token(symbol, classification)
-                    print(tk.element, tk.classification)
-
-                if delTok == "(":
-                    symbol += delTok
-                    classification = "opening parentheses"
-                    tk = t.Token(symbol, classification)
-                    print(tk.element, tk.classification)
-
-                if delTok == ")":
-                    symbol += delTok
-                    classification = "closing parentheses"
-                    tk = t.Token(symbol, classification)
-                    print(tk.element, tk.classification)
-
-                if delTok in operators:
-                    symbol += delTok
-                    classification = "operator"
-                    tk = t.Token(symbol, classification)
-                    print(tk.element, tk.classification)
-                
-                symbol = ""
-                delTok = ""
-
-                if element in alphabet and element.islower():
-                    symbol += element
+                if token[curr] in alphabet and token[curr].islower():
+                    symbol += token[curr]
+                    curr += 1 
                     state = 1
-
-                # break
             case 1:
-
-                if element in alphabet or element.isdigit():
-                    symbol += element
+                if token[curr] in alphabet or token[curr].isdigit():
+                    symbol += token[curr]
+                    curr += 1
                 else:
                     if symbol in keyWords:
                         classification = "keyword"
-                    elif symbol in types:
-                        classification = "type"
-                    else:
-                        classification = "identifier"
-                    tk = t.Token(symbol, classification)
-                    symbol = ""
-                    delTok += element
-                    print(tk.element, tk.classification)
-                    state = 0
-
-                # break
-
-            
